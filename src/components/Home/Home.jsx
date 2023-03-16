e1305aeba188f0276a51eabe2ee4d5dc6acef28d
root <berthierclementpro@gmail.com>
2023-03-16 17:12:42 +0100

    in construc in home

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,31 @@
+import './Home.scss'
+
+function Home() {
+    const title = "In Construct"
+    const titleArray = title.split(/(?!^)/u)
+    console.log("titleArray", titleArray);
+ 
+  return (
+    <div className="home">
+        <div className="left">
+            <h1 className='home_title'>
+                {titleArray.map((letter, index) => {
+                    return (
+                        <span key={index} className={`letter_${index}`}>
+                            {letter}
+                        </span>
+                    )
+                })}
+            </h1>
+        </div>
+        <div className="center">
+
+        </div>
+        <div className="right">
+            <h1 className="home_title">About Me</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Home
