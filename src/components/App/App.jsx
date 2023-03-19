02aeb3373e0225833a2350e11a485e7313b07db6
ClementBerthier <berthierclementpro@gmail.com>
2023-03-19 18:38:56 +0100

    modify language by french

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,13 @@
-import {Routes, Route} from 'react-router-dom'
-import Home from '../Home/Home'
-import './App.scss'
+import { Routes, Route } from "react-router-dom";
+import Home from "../Home/Home";
+import "./App.scss";
 
 function App() {
- 
   return (
     <Routes>
       <Route path="/" element={<Home />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,31 +1,31 @@
-import './Home.scss'
+import "./Home.scss";
 
 function Home() {
-    const title = "In Construction"
-    const titleArray = title.split(/(?!^)/u)
-    console.log("titleArray = ", titleArray);
- 
+  const title = "En Développement";
+  const titleArray = title.split(/(?!^)/u);
+  console.log("titleArray = ", titleArray);
+
   return (
     <div className="home">
+      <div className="home_container">
         <div className="left">
-            <h1 className='home_title'>
-                {titleArray.map((letter, index) => {
-                    return (
-                        <span key={index} className={`letter_${index}`}>
-                            {letter}
-                        </span>
-                    )
-                })}
-            </h1>
+          <h1 className="home_title">
+            {titleArray.map((letter, index) => {
+              return (
+                <span key={index} className={`letter_${index}`}>
+                  {letter}
+                </span>
+              );
+            })}
+          </h1>
         </div>
-        <div className="center">
-
-        </div>
+        <div className="center"></div>
         <div className="right">
-            <h1 className="home_title">About Me</h1>
+          <h1 className="home_title">A propos de moi</h1>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
