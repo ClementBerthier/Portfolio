9e20208d4acb87ab3c82ed8b47ea4f91a656cfa4
ClementBerthier <berthierclementpro@gmail.com>
2023-04-04 16:34:11 +0200

    add cast shadow on text in home

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import { Portfolio } from "../Portfolio/Portfolio";
 import "../../styles/App.scss";
+import { AboutMe } from "../AboutMe/AboutMe.jsx";
 
 export const App = () => {
     return (
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useRef } from "react";
 import "../../styles/Home.scss";
 import { Context } from "../Context/Context.jsx";
 
@@ -8,12 +8,34 @@ export const Home = () => {
     const hidden = isClicked ? "" : "hidden";
     const hiddenButton = isClicked ? "hidden" : "";
 
+    const titleRef = useRef();
+    const subtitleRef = useRef();
+    const descriptionRef = useRef();
+
+    const elements = [titleRef, subtitleRef, descriptionRef];
+
+    const calcShadow = (event) => {
+        elements.forEach((element) => {
+            const rect = element.current.getBoundingClientRect();
+            const x = event.clientX - rect.left;
+            const y = event.clientY - rect.top;
+            const offsetX = -(x - rect.width / 2) / 10;
+            const offsetY = -(y - rect.height / 2) / 10;
+            const shadow = `${offsetX}px ${offsetY}px 10px rgba(0, 0, 0, 0.5)`;
+
+            element.current.style.textShadow = shadow;
+        });
+    };
+    const handleMouseMove = (event) => {
+        calcShadow(event);
+    };
+
     const handleClick = () => {
         setIsClicked(true);
     };
 
     return (
-        <section className="home" id="home">
+        <section className="home" id="home" onMouseMove={handleMouseMove}>
             <div className="trigger-container">
                 <button
                     className={`trigger ${hiddenButton}`}
@@ -23,12 +45,15 @@ export const Home = () => {
                 </button>
             </div>
             <div className={`home-container ${hidden}`}>
-                <h1 className="title">Bienvenue sur mon Portfolio</h1>
-                <h2 className="subtitle">Je suis Clément Berthier</h2>
-                <p className="description">
+                <h1 ref={titleRef} className="title">
+                    Bienvenue sur mon Portfolio
+                </h1>
+                <h2 ref={subtitleRef} className="subtitle">
+                    Je suis Clément Berthier
+                </h2>
+                <p ref={descriptionRef} className="description">
                     Développeur fullstack Web et Web Mobile
                 </p>
-                <button className="button">en savoir +</button>
             </div>
         </section>
     );
