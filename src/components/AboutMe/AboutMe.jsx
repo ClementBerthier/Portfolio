012bd3b58cd004e4c777106a5abc157f25d9296f
ClementBerthier <berthierclementpro@gmail.com>
2023-04-12 17:10:45 +0200

    archi finish for binder about me

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -10,24 +10,6 @@ import { useState } from "react";
 export const AboutMe = () => {
     const [open, setOpen] = useState(false);
 
-    const skillsSection = [
-        {
-            id: 1,
-            name: "Frontend",
-            variable: logoSkillsFrontend,
-        },
-        {
-            id: 2,
-            name: "Backend",
-            variable: logoSkillsBackend,
-        },
-        {
-            id: 3,
-            name: "Autres",
-            variable: logoSkillsOther,
-        },
-    ];
-
     return (
         <section className="aboutMe" id="aboutMe">
             <div className="presentation">
@@ -58,29 +40,118 @@ export const AboutMe = () => {
                     </p>
                 </div>
             </div>
-            <div className="timeline">
-                {data.map((data) => (
-                    <Modal
-                        closeIcon
-                        open={open}
-                        trigger={
-                            <div>
-                                <time className="time"></time>
-                                <p className="description"></p>
-                            </div>
-                        }
-                        onClose={() => setOpen(false)}
-                        onOpen={() => setOpen(true)}
-                    >
-                        <Header icon="archive" content="Archive Old Messages" />
-                        <Modal.Content>
-                            <p>
-                                Your inbox is getting full, would you like us to
-                                enable automatic archiving of old messages?
-                            </p>
-                        </Modal.Content>
-                    </Modal>
-                ))}
+            <div className="bottom-container">
+                <div className="timeline">
+                    {data.map((data) => (
+                        <Modal
+                            className="modal-timeline"
+                            closeIcon
+                            key={data.id}
+                            open={open === data.id}
+                            trigger={
+                                <div className="trigger-container">
+                                    <time className="time">{data.date}</time>
+                                    <p className="description">{data.role}</p>
+                                </div>
+                            }
+                            onClose={() => setOpen(null)}
+                            onOpen={() => setOpen(data.id)}
+                        >
+                            <Header content={data.role} />
+                            <Modal.Content>
+                                <div>
+                                    <p>{data.description}</p>
+                                    <p>{data.date}</p>
+                                    <p>{data.school}</p>
+                                    <p>{data.place}</p>
+                                </div>
+                            </Modal.Content>
+                        </Modal>
+                    ))}
+                </div>
+                <div className="skills">
+                    <div className="frontend">
+                        <h1 className="title">Frontend</h1>
+                        <div className="logo">
+                            {logoSkillsFrontend.map((logo) => (
+                                <a
+                                    href={logo.url}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    <Popup
+                                        position="top center"
+                                        size="huge"
+                                        className="popup"
+                                        content={logo.name}
+                                        trigger={
+                                            <img
+                                                className="logoFront"
+                                                key={logo.id}
+                                                src={logo.path}
+                                                alt={logo.name}
+                                            />
+                                        }
+                                    />
+                                </a>
+                            ))}
+                        </div>
+                    </div>
+                    <div className="backend">
+                        <h1 className="title">Backend</h1>
+                        <div className="logo">
+                            {logoSkillsBackend.map((logo) => (
+                                <a
+                                    href={logo.url}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    <Popup
+                                        position="top center"
+                                        size="huge"
+                                        className="popup"
+                                        content={logo.name}
+                                        trigger={
+                                            <img
+                                                className="logoBack"
+                                                key={logo.id}
+                                                src={logo.path}
+                                                alt={logo.name}
+                                            />
+                                        }
+                                    />
+                                </a>
+                            ))}
+                        </div>
+                    </div>
+                    <div className="tools">
+                        <h1 className="title">Outils</h1>
+                        <div className="logo">
+                            {logoSkillsOther.map((logo) => (
+                                <a
+                                    href={logo.url}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    <Popup
+                                        position="top center"
+                                        size="huge"
+                                        className="popup"
+                                        content={logo.name}
+                                        trigger={
+                                            <img
+                                                className="logoTools"
+                                                key={logo.id}
+                                                src={logo.path}
+                                                alt={logo.name}
+                                            />
+                                        }
+                                    />
+                                </a>
+                            ))}
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
     );
