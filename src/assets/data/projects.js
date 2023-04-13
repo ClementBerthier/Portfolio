3f441b130d36358627524fc70e4cbdf3abd93c02
ClementBerthier <berthierclementpro@gmail.com>
2023-04-13 18:56:20 +0200

    hover img ok

--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -1,10 +1,11 @@
 export const projects = [
     {
         id: 1,
-        name: "Node.js",
+        name: "Jeu de carte Kaamelott",
         path: "/images/projects/space4.png",
         url: "https://nodejs.org/en",
-        description: "Moi en général je repond merde, ca va avec tout",
+        description:
+            "On plaisante, on plaisante… Ah ah Sire! Je vous attends! À moins que vous préfériez qu’on dise partout que le roi est une petite pédale qui pisse dans son froc à l’idée d’se battre! Vous savez c’est quand de même pas grave de pas savoir faire des tartes! Ben évidemment que c’est vrai! Vous avez jamais dit que ça devait être faux!",
         //TODO: mettre les vrais urls des projets
     },
     {
@@ -12,7 +13,8 @@ export const projects = [
         name: "Node.js",
         path: "/images/projects/space2.png",
         url: "https://nodejs.org/en",
-        description: "Le caca des pigeon c'est caca",
+        description:
+            "Ils chantent pas vos espions. Déjà à la corne, ils regardent même pas vers ici! Vous pouvez bien agiter tout les drapeaux que vous voudrez! Mais on en entend parler dans les tavernes à ivrognes! Voilà! Ben attendez, je vais vous rendre la vôtre.",
     },
     {
         id: 3,
@@ -20,7 +22,7 @@ export const projects = [
         path: "/images/projects/space3.png",
         url: "https://nodejs.org/en",
         description:
-            "pour savoir si il va y avoir du vent il faut mettre son doight dans le cul du coq",
+            "Y en a marre de se comporter comme des sagouins avec tout le monde sous prétexte qu’on a des responsabilités. Mais… C’est le Chevalier de Provence ou le Chevalier Gaulois? Faudrait savoir! N’empêche que tout le monde parle de moi! C’est quand même un signe! Non mais n’exagérez pas non plus! J’vous demande quand même pas de manger des briques!",
     },
     {
         id: 4,
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -2,8 +2,22 @@ import { projects } from "../../assets/data/projects";
 import { Carousel } from "react-responsive-carousel";
 import "../../styles/Projects.scss";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Button } from "semantic-ui-react";
+import { useState } from "react";
 
 export const Projects = () => {
+    const [mouseEnter, setMouseEnter] = useState(false);
+
+    const handleMouseEnter = () => {
+        setMouseEnter(true);
+    };
+
+    const handleMouseLeave = () => {
+        setMouseEnter(false);
+    };
+
+    console.log("ishovered", mouseEnter);
+
     return (
         <section className="projects" id="projects">
             <div className="title-container">
@@ -12,24 +26,40 @@ export const Projects = () => {
             <div className="carousel-container">
                 <Carousel showThumbs={false} showIndicators={false}>
                     {projects.map((project) => (
-                        <div className="image-container" key={project.id}>
+                        <div
+                            className="image-container"
+                            key={project.id}
+                            onMouseEnter={handleMouseEnter}
+                            onMouseLeave={handleMouseLeave}
+                        >
                             <img
                                 className="image-project"
                                 src={project.path}
                                 alt={project.name}
                             />
-                            {/* <div>
-                                <h3>{project.name}</h3>
-                                <span>
-                                    {project.id}/{projects.length}
-                                </span>
-                                <p>{project.description}</p>
-                            </div> */}
+
+                            <div
+                                className={`info-container ${
+                                    !mouseEnter ? "hidden" : ""
+                                }`}
+                            >
+                                <div className="title-container">
+                                    <h3 className="info-title">
+                                        {project.name}
+                                    </h3>
+                                    <span className="number">
+                                        {project.id}/{projects.length}
+                                    </span>
+                                </div>
+                                <div className="description">
+                                    <p>{project.description}</p>
+                                </div>
+                            </div>
                         </div>
                     ))}
                 </Carousel>
             </div>
-            <button>en voir +</button>
+            <Button className="seeMore">en voir +</Button>
         </section>
     );
 };
