c2b99735caada5660e1ec9c13b95aff69e298845
ClementBerthier <berthierclementpro@gmail.com>
2023-04-17 10:38:24 +0200

    add arrowup on footer to scroll up

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,10 +2,22 @@ import { logoFooter } from "../../assets/data/logoFooter.js";
 import "../../styles/Footer.scss";
 
 export const Footer = () => {
+    const handleClick = () => {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    };
+
     return (
         <footer className="footer" id="footer">
             <div className="box">
                 <div className="box-top">
+                    <div className="return">
+                        <img
+                            className="arrowUp"
+                            src="/images/arrowUp.png"
+                            alt="Arrow"
+                            onClick={handleClick}
+                        />
+                    </div>
                     <div className="myName">Clement Berthier</div>
                     <div className="link">
                         <div className="logo-container">
