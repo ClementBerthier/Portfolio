2fe18a8b4ed44f4ecf0ccb0e1df0515f2692fb21
ClementBerthier <berthierclementpro@gmail.com>
2023-04-20 18:26:09 +0200

    MaJ picture project

--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -2,21 +2,12 @@ export const projects = [
     {
         id: 1,
         name: "Mon Portfolio",
-        path: "/images/projects/space4.png",
-        url: "https://nodejs.org/en",
-        description:
-            "On plaisante, on plaisante… Ah ah Sire! Je vous attends! À moins que vous préfériez qu’on dise partout que le roi est une petite pédale qui pisse dans son froc à l’idée d’se battre! Vous savez c’est quand de même pas grave de pas savoir faire des tartes! Ben évidemment que c’est vrai! Vous avez jamais dit que ça devait être faux!",
-        //TODO: mettre les vrais urls des projets
-    },
-    {
-        id: 2,
-        name: "Mon Portfolio",
-        path: "/images/projects/space2.png",
+        path: "/images/projects/Portfolio.png",
         url: "https://portfolio-kohl-ten-27.vercel.app/",
         description: "La premiere version de mon portfolio, réalisé en React",
     },
     {
-        id: 3,
+        id: 2,
         name: "MyAmiDice",
         path: "/images/projects/MyAmiDice.png",
         url: "https://github.com/ClementBerthier/MyAmiDice",
@@ -24,7 +15,7 @@ export const projects = [
             "MyAmiDice est un projet de fin de formation réalisé en équipe de 5 personnes (2 front-end, 3 back-end). Ce projet est une plateforme de jeu de rôle en ligne permettant à un groupe de joueurs de réaliser une partie de jeu de rôle sur une interface dédiée.",
     },
     {
-        id: 4,
+        id: 3,
         name: "OctoDiving",
         path: "/images/projects/octodiving.png",
         url: "https://www.octodiving.fr/",
