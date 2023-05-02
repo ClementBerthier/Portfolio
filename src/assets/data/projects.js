5cade7701b3432958f25739560a22a7260b4d2b0
ClementBerthier <berthierclementpro@gmail.com>
2023-05-02 18:46:15 +0200

    'add new projet

--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -1,13 +1,21 @@
 export const projects = [
     {
         id: 1,
+        name: "LePlacard",
+        path: "/images/projects/LePlacard.png",
+        url: "https://github.com/ClementBerthier/LePlacard",
+        description:
+            "LePlacard est un site web permettant à un joueur ou collectionneur de figurines Warhammer de visualiser facilement son stock, mais aussi de tenir à jour l'avancement des différents états de ses figurines (nettoyage, peinture, vernis, etc.). \n Ce projet est en cours de développement.",
+    },
+    {
+        id: 2,
         name: "Mon Portfolio",
         path: "/images/projects/Portfolio.png",
         url: "https://portfolio-kohl-ten-27.vercel.app/",
         description: "La premiere version de mon portfolio, réalisé en React",
     },
     {
-        id: 2,
+        id: 3,
         name: "MyAmiDice",
         path: "/images/projects/MyAmiDice.png",
         url: "https://github.com/ClementBerthier/MyAmiDice",
@@ -15,7 +23,7 @@ export const projects = [
             "MyAmiDice est un projet de fin de formation réalisé en équipe de 5 personnes (2 front-end, 3 back-end). Ce projet est une plateforme de jeu de rôle en ligne permettant à un groupe de joueurs de réaliser une partie de jeu de rôle sur une interface dédiée.",
     },
     {
-        id: 3,
+        id: 4,
         name: "OctoDiving",
         path: "/images/projects/octodiving.png",
         url: "https://www.octodiving.fr/",
